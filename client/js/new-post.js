3579c1fb6edd6a39b189af0c669c9c44aa96639b
Trevan Hetzel <trevan@hetzelcreative.com>
2014-06-18 09:33:04 -0500

    Fix Submit form

--- client/js/new-post.js
+++ client/js/new-post.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
-	var btn = '#submit';
+	var btn = '#submit',
+		formSubmit = '#formSubmit';
 
 	$(document).on('click', btn, function (e) {
 		e.preventDefault();
 
 		$(this).toggleClass('btn-open')
 
-		$(document).find('.submit').toggleClass('form-shown');
+		$(document).find('#submitDiv').toggleClass('form-shown');
+	});
+
+	$(document).on('click', formSubmit, function () {
+		$(btn).removeClass('btn-open');
+		$(document).find('#submitDiv').removeClass('form-shown');
 	});
 });
