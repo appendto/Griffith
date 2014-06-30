6a9ab6c4129b5a1be7803a8c5611148d8ac4d91a
jakerella <jordan@jordankasper.com>
2014-06-30 08:57:59 -0500

    Fixed root url env variable for node app

--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@ tar xzf griffith.tgz
 
 export PORT=80
 export MONGO_URL=mongodb://localhost:27017/griffith
-export ROOT_URL=http://162.243.74.40
+export ROOT_URL=http://griffith.a2staging.com
 
 forever start bundle/main.js
